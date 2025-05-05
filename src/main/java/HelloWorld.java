d5a5a2588b2c9b282a896977e7a1cf257e9af209
Pier Costante Babini <piercostante.babini@studio.unibo.it>
2025-05-05 11:43:28 +0200

    Done first 5 tasks

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is a simple Java program.");
+        System.out.println("It prints messages to the console.");
+        System.out.println("Goodbye!");
+    }
+
+}
